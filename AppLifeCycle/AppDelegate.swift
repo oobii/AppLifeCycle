2f748cb8e612dab3472a8856a25058b5bbb1b08e
martynov <vvayfarer@yandex.com>
2017-12-26 15:08:09 -0500

    first commit

--- AppLifeCycle/AppDelegate.swift
+++ AppLifeCycle/AppDelegate.swift
@@ -13,32 +13,64 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+/*
+ 
+     Run the app in Simulator. When you open the app, you'll see two messages print to the console:
+      
+     Did finish launching.
+     Did become active.
+      
+     Press Shift-Command-H to simulate pressing the Home button and dismissing the app. You'll see two more messages print to the console:
+      
+     Will resign active.
+     Did enter background.
+      
+     Open the app switcher by pressing Shift-Command-H two times, and reopen the app. You'll see another two messages print to the console:
+      
+     Will enter foreground.
+     Did become active.
+      
+     Open the app switcher again, then return to the app.
+      
+     Will resign active.
+     Did become active.
+     
+ */
+    
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("Did finish launching")
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("Will resign active")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("Did enter background")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("Will enter foreground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("Did become active")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("Will terminate")
     }
 
 
